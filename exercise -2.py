00a62a4063dd95e5930fc784a2a01dc2d691b2a1
cjstokes91 <59268316+cjstokes91@users.noreply.github.com>
2020-03-10 13:22:32 -0700

    update number 4

---
+++ exercise -2.py
@@ -0,0 +1,6 @@
+exercise 2 
+phrase = input("Please enter a word or phrase: ")
+
+while phrase != "quit":
+    print(f"What you entered is {len(phrase)} characters long")
+    phrase = input("Please enter a word or phrase: ")
---
+++ exercise-1.py
@@ -0,0 +1,8 @@
+#exercise 1
+if letter in 'a e i o u':   
+ print(f"The letter {letter} is a vowel")
+else:   
+ print(f"The letter {letter} is a consonant")
+
+
+
